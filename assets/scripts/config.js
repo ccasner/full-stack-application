c15f3d10ab3e2aef046f43acedf5ad70739fc982
ccasner <charlottecasner@gmail.com>
2017-09-27 17:08:27 -0400

    Fix api link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://protected-hollows-11558.herokuapp.com/',
-    development: 'http://localhost:4741'
+    production: 'https://protected-hollows-11558.herokuapp.com'
+    // development: 'http://localhost:4741'
   },
   googleMaps: {
     production: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDMVv5sfLKk5TDsNYNTaYuu9QpGeIa3VzY&callback=initMap'
